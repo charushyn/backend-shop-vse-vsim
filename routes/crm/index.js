663048939678623e1adffb5ec634b9e8cea8fde8
Illia Charushyn <charushyngg@gmail.com>
2024-12-02 04:13:10 +0100

    1.0.1

--- routes/crm/index.js
+++ routes/crm/index.js
@@ -1,23 +1,19 @@
-import express from 'express'
-import 'dotenv/config'
-import { createProxyMiddleware } from 'http-proxy-middleware';
+import express from "express";
+import "dotenv/config";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 import { configDotenv } from "dotenv";
 
 configDotenv();
 
-const allowedEndPoints = [
-  '/products/categories',
-  '/products'
-]
+const allowedEndPoints = ["/products/categories", "/products"];
 
-const proxyFilter = function(path, req){
-  if(req.method === "GET"){
-    return true
-  } else {
-    return false
+const proxyFilter = function (path, req) {
+  if (req.method === "GET") {
+    return true;
   }
-}
+  return true;
+};
 
 const proxyMiddleware = createProxyMiddleware({
   target: `${process.env.CRM_URL}`,
@@ -25,14 +21,14 @@ const proxyMiddleware = createProxyMiddleware({
   pathFilter: proxyFilter,
   on: {
     proxyReq: (proxyReq) => {
-      proxyReq.setHeader('Authorization', `Bearer ${process.env.CRM_TOKEN}`)
-      proxyReq.setHeader('Content-Type', 'application/json')
-    }
-  }
-})
+      proxyReq.setHeader("Authorization", `Bearer ${process.env.CRM_TOKEN}`);
+      proxyReq.setHeader("Content-Type", "application/json");
+    },
+  },
+});
 
-const crmRoute = express.Router()
+const crmRoute = express.Router();
 
-crmRoute.use('/crm', proxyMiddleware)
+crmRoute.use("/crm", proxyMiddleware);
 
 export default crmRoute;
